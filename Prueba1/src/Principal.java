b7acd346e58a7a499c6b1adc4768d81f788bf06c
at <at@PC-12>
2020-11-19 18:10:07 +0100

    NuevoCommit

--- Prueba1/src/Principal.java
+++ Prueba1/src/Principal.java
@@ -2,7 +2,7 @@
 public class Principal {
 
 	public static void main(String[] args) {
-		System.out.println("Hola mundo Java-Git");
+		System.out.println("Hola mundo Java-Git (nuevo commit");
 
 	}
 
